b3737d497bf9197b22e5affb155b2f0b14ad059f
Ivan Romero <E154670@ameren.com>
2019-07-29 00:14:59 -0500

    Mobile look and refinement (added button to change themes, non-functional)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  constructor() {}
+
   title = 'Ivan Romero';
   backgroundImage = 'assets/background.jpg';
   altBackgroundImage = 'assets/mohawk_lake.jpg';
   logo = 'assets/logo72.png';
+  currentTheme = 'Light Theme';
+  desktop = true;
+  innerWidth: any;
+
+  ngOnInit() {
+    this.innerWidth = window.innerWidth;
+    console.log( innerWidth );
+    if (this.innerWidth <= 500) {
+      this.desktop = false;
+    }
+  }
 
   scrollToElement($element): void {
     $element.scrollIntoView({
@@ -18,4 +31,12 @@ export class AppComponent {
       inline: 'start'
     });
   }
+
+  turnToDarkTheme() {
+    console.log('turning to dark theme');
+  }
+
+
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { AboutMeComponent } from './about-me/about-me.component';
 
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 import { AppComponent } from './app.component';
 import { ResumeComponent } from './resume/resume.component';
@@ -15,6 +16,7 @@ import { MatCardModule } from '@angular/material/card';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatListModule } from '@angular/material/list';
 import {MatProgressBarModule} from '@angular/material/progress-bar';
+import {MatIconModule} from '@angular/material/icon';
 
 import { FlexLayoutModule } from '@angular/flex-layout';
 
@@ -28,6 +30,7 @@ import { FlexLayoutModule } from '@angular/flex-layout';
   ],
   imports: [
     BrowserModule,
+    CommonModule,
     BrowserAnimationsModule,
     MatToolbarModule,
     MatButtonModule,
@@ -35,7 +38,8 @@ import { FlexLayoutModule } from '@angular/flex-layout';
     FlexLayoutModule,
     MatGridListModule,
     MatListModule,
-    MatProgressBarModule
+    MatProgressBarModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
