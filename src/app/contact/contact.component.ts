8df4dc388d75168d3d444170ca7dca2bb8c6ca3d
Ivan Romero <ivanrr308@gmail.com>
2019-07-31 14:04:09 -0500

    Validators on form started

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'contact',
@@ -7,23 +7,21 @@ import { FormGroup, FormBuilder } from '@angular/forms';
   styleUrls: ['./contact.component.scss']
 })
 export class ContactComponent implements OnInit {
-  // contactForm: FormGroup;
-  options: FormGroup;
-  constructor(fb: FormBuilder) {
-    this.options = fb.group({
-      hideRequired: false,
-      floatLabel: 'auto'
-    });
-    // this.createForm();
+  angForm: FormGroup;
+  constructor(private fb: FormBuilder) {
+    this.createForm();
   }
-  // createForm() {
-  //   this.contactForm = this.fb.group({
-  //     email: [''],
-  //     password: ['']
-  //   });
-  // }
-  // onClickSubmit(name, email) {
-  //   alert('Your name is : ' + name + 'Your email is: ' + email);
-  // }
-  ngOnInit() {}
+
+  createForm() {
+    this.angForm = this.fb.group({
+      name: ['', Validators.required ],
+      email: ['', Validators.required ],
+      subject: ['', Validators.required ],
+      message: ['', Validators.required]
+    });
+  }
+
+  ngOnInit() {
+  }
 }
+
