fdc24b00bfe26b191c80b2c53ec99eb00822c16b
Ivan Romero <ivanrr308@gmail.com>
2019-08-26 00:00:09 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,15 +1,17 @@
-//Install express server
+import express from 'express';
+
+
 const express = require('express');
 const path = require('path');
 
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/my-resume'));
+app.use(express.static(__dirname + './dist/my-resume'));
 
 app.get('/*', function(req,res) {
 
-res.sendFile(path.join(__dirname+'/dist/my-resume/index.html'));
+res.sendFile(path.join(__dirname+'./dist/my-resume/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
